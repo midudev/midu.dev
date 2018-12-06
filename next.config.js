43e695365db814a7b9beaae92a7c6a17d639dc68
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-12-06 23:31:04 +0100

    Add remark-external-links plugin

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 const {withNextein} = require('nextein/config')
 
-module.exports = withNextein({})
+module.exports = withNextein({
+  nextein: {
+    plugins: [
+      ['nextein-plugin-markdown', {remark: ['remark-external-links']}]
+      // your nextein plugins here
+    ]
+  }
+  // Your own next.js config here
+})
