f809dd52c85ddd3f9c3734914e7523a24acbc186
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-12-06 22:55:51 +0100

    Add twitter to preconnect

--- components/Head/Preconnect.js
+++ components/Head/Preconnect.js
@@ -8,5 +8,6 @@ export default () => (
       rel="preconnect dns-prefetch"
       href="https://www.google-analytics.com"
     />
+    <link rel="preconnect dns-prefetch" href="https://platform.twitter.com" />
   </>
 )
