e7855dfb5c7563cbbf74b9717bc3982bada76e6c
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-12-10 11:30:09 +0100

    Add rehype prism transformation

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const {withNextein} = require('nextein/config')
 module.exports = withNextein({
   nextein: {
     plugins: [
-      ['nextein-plugin-markdown', {remark: ['remark-external-links']}]
+      [
+        'nextein-plugin-markdown',
+        {
+          remark: ['remark-external-links'],
+          rehype: ['@mapbox/rehype-prism']
+        }
+      ]
       // your nextein plugins here
     ]
   }
