a03854bdf3beb98dd3372c6bba345f5325f2c9f0
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-12-10 11:35:06 +0100

    New simpler and colorful header

--- components/Header/LogoHeader.js
+++ components/Header/LogoHeader.js
@@ -5,27 +5,13 @@ const CodeIcon = () => (
     </svg>
     <style jsx>{`
       svg {
-        fill: #0099ff;
-        margin-right: 4px;
-      }
-    `}</style>
-  </React.Fragment>
-)
-
-const GoHomeIcon = () => (
-  <React.Fragment>
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-      <path d="M19.871 12.165l-8.829-9.758A1.392 1.392 0 0 0 10 1.937c-.397 0-.767.167-1.042.47L.129 12.165a.5.5 0 0 0 .741.67l2.129-2.353V18.5c0 .827.673 1.5 1.5 1.5h11c.827 0 1.5-.673 1.5-1.5v-8.018l2.129 2.353a.499.499 0 1 0 .741-.671zM12 19H8v-4.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V19zm4-.5a.5.5 0 0 1-.5.5H13v-4.5c0-.827-.673-1.5-1.5-1.5h-3c-.827 0-1.5.673-1.5 1.5V19H4.5a.5.5 0 0 1-.5-.5V9.377l5.7-6.3c.082-.091.189-.141.3-.141s.218.05.3.141l5.7 6.3V18.5z" />
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-      <path d="M.646 10.146l6-6a.5.5 0 0 1 .707.707L2.207 9.999H18.5a.5.5 0 0 1 0 1H2.207l5.146 5.146a.5.5 0 0 1-.708.707l-6-6a.5.5 0 0 1 0-.707z" />
-    </svg>
-    <style jsx>{`
-      svg {
-        margin-right: 4px;
-      }
-      svg + svg {
-        margin-right: 16px;
+        background: #0099ff;
+        border-radius: 16px;
+        display: inline-block;
+        fill: #fff;
+        margin-right: 8px;
+        padding: 4px;
+        transform: scale(1.1);
       }
     `}</style>
   </React.Fragment>
@@ -34,38 +20,23 @@ const GoHomeIcon = () => (
 export default () => (
   <React.Fragment>
     <a href="/">
-      <span>
-        <GoHomeIcon /> midudev
-      </span>
-      <div>
-        <CodeIcon />frontend, performance & more...
-      </div>
+      <CodeIcon />
+      <span>midudev</span>
     </a>
     <style jsx>{`
       a {
         color: #000;
-        display: block;
+        display: flex;
         font-size: 24px;
         font-weight: 300;
-        line-height: 20px;
+        justify-content: center;
+        line-height: 28px;
         overflow: hidden;
         padding: 16px;
-        text-decoration: none;
+        transition: opacity 0.3s ease;
       }
-      a:hover span {
-        transform: translateX(0px);
-      }
-      div {
-        align-items: center;
-        color: #09f;
-        display: flex;
-        font-size: 12px;
-        height: 20px;
-      }
-      span {
-        display: block;
-        transform: translateX(-65px);
-        transition: all 0.3s ease-in-out;
+      a:hover {
+        opacity: 0.65;
       }
     `}</style>
   </React.Fragment>
