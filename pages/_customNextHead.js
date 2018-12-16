99a4ce1bf22231e8a1e11544bdfac1e7f99149af
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-12-16 18:45:58 +0100

    Remove not used stuff

--- pages/_customNextHead.js
+++ pages/_customNextHead.js
@@ -18,89 +18,8 @@ class Head extends Component {
     _documentProps: PropTypes.any
   }
 
-  static propTypes = {
-    nonce: PropTypes.string,
-    preload: PropTypes.bool
-  }
-
-  getCssLinks() {
-    const {assetPrefix, files} = this.context._documentProps
-    if (!files || files.length === 0) {
-      return null
-    }
-
-    return files.map(file => {
-      // Only render .css files here
-      if (!/\.css$/.exec(file)) {
-        return null
-      }
-
-      return (
-        <link
-          key={file}
-          nonce={this.props.nonce}
-          rel="stylesheet"
-          href={`${assetPrefix}/_next/${file}`}
-        />
-      )
-    })
-  }
-
-  getPreloadDynamicChunks() {
-    const {dynamicImports, assetPrefix} = this.context._documentProps
-    return dynamicImports.map(bundle => {
-      return (
-        <link
-          rel="preload"
-          key={bundle.file}
-          href={`${assetPrefix}/_next/${bundle.file}`}
-          as="script"
-          nonce={this.props.nonce}
-        />
-      )
-    })
-  }
-
-  getPreloadMainLinks() {
-    const {assetPrefix, files} = this.context._documentProps
-    if (!files || files.length === 0) {
-      return null
-    }
-
-    return files.map(file => {
-      // Only render .js files here
-      if (!/\.js$/.exec(file)) {
-        return null
-      }
-
-      return (
-        <link
-          key={file}
-          nonce={this.props.nonce}
-          rel="preload"
-          href={`${assetPrefix}/_next/${file}`}
-          as="script"
-        />
-      )
-    })
-  }
-
   render() {
-    function getPagePathname(page) {
-      if (page === '/') {
-        return '/index.js'
-      }
-
-      return `${page}.js`
-    }
-    const {
-      head,
-      styles,
-      assetPrefix,
-      __NEXT_DATA__
-    } = this.context._documentProps
-    const {page, buildId} = __NEXT_DATA__
-    const pagePathname = getPagePathname(page)
+    const {head, styles} = this.context._documentProps
 
     let children = this.props.children
     // show a warning if Head contains <title> (only in development)
@@ -118,34 +37,6 @@ class Head extends Component {
     return (
       <head {...this.props}>
         {head}
-        {this.props.preload && (
-          <>
-            {page !== '/_error' && (
-              <link
-                rel="preload"
-                href={`${assetPrefix}/_next/static/${buildId}/pages${pagePathname}`}
-                as="script"
-                nonce={this.props.nonce}
-              />
-            )}
-            <link
-              rel="preload"
-              href={`${assetPrefix}/_next/static/${buildId}/pages/_app.js`}
-              as="script"
-              nonce={this.props.nonce}
-            />
-            <link
-              rel="preload"
-              href={`${assetPrefix}/_next/static/${buildId}/pages/_error.js`}
-              as="script"
-              nonce={this.props.nonce}
-            />
-            {this.getPreloadDynamicChunks()}
-            {this.getPreloadMainLinks()}
-          </>
-        )}
-
-        {this.getCssLinks()}
         {styles || null}
         {children}
       </head>
