9245b6999373305fbdce365cafb0687f422b6d73
midudev <miduga@gmail.com>
2019-03-21 14:09:16 +0100

    Cache images

--- static/sw.js
+++ static/sw.js
@@ -1,11 +1,12 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js')
 
-
 workbox.googleAnalytics.initialize()
 
 workbox.routing.registerRoute(
-  /\.(?:png|jpg|jpeg|svg|gif|js|css|html)$/,
-  new workbox.strategies.NetworkFirst()
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'images-cache'
+  })
 )
 
 // workbox.routing.registerRoute(
