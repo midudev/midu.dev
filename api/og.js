7bf8e2f77dea3ad914ef03ef8119812636e873ad
Miguel Ángel Durán <miduga@gmail.com>
2022-10-14 17:09:43 -0500

    Use new way to generate images

--- api/og.js
+++ api/og.js
@@ -1,3 +1,70 @@
+import { Resvg } from '@resvg/resvg-js'
+import satori from 'satori'
+import { html } from 'satori-html'
+
+export default async (req, res) => {
+  // get query string from req
+  const { query } = req
+  // transform to object query
+  const { title, tag } = query
+
+  const opts = {
+    background: '#fff',
+    fitTo: {
+      mode: 'width',
+      value: 1200
+    }
+  }
+
+  const inter = await fetch('https://midu.dev/inter.ttf').then((res) =>
+    res.arrayBuffer()
+  )
+
+  const markup = html`<div
+    style="display: flex; flex-direction: column; width: 100vw; height: 100vh; padding: 64px;background-image: url('https://midu.dev/images/blur-background-01.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat;"
+  >
+    <img
+      id="logo"
+      src="https://midu.dev/images/solo-logo.png"
+      alt="Solo logo"
+      style="width: 150px;"
+    />
+    <img
+      id="tag"
+      src="https://midu.dev/images/tags/${tag}.png"
+      alt="${tag} logo"
+      style="width: 600px; position: absolute; bottom: -100px; right: -100px; opacity: .3;"
+    />
+    <h1
+      style="font-size: 128px; color: #111; max-width: 70%; text-align: left; line-height: 105%; letter-spacing: -2px;"
+    >
+      ${title}
+    </h1>
+  </div>`
+
+  const svg = await satori(markup, {
+    width: 1200,
+    height: 630,
+    fonts: [
+      {
+        name: 'Inter',
+        data: inter,
+        weight: 400,
+        style: 'normal'
+      }
+    ]
+  })
+
+  const resvg = new Resvg(svg, opts)
+
+  const pngData = resvg.render()
+  const pngBuffer = pngData.asPng()
+
+  res.setHeader('Content-Type', 'image/png')
+  res.status(200).send(pngBuffer)
+}
+
+/*
 import chromium from 'chrome-aws-lambda'
 import playwright from 'playwright-core'
 
@@ -49,3 +116,4 @@ export default async (req, res) => {
     }
   }
 }
+*/
