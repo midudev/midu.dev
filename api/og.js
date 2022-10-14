4a3d5b332e987a62fab0235862b94fea7da1cbcb
Miguel Ángel Durán <miduga@gmail.com>
2022-10-14 17:17:55 -0500

    Remove old code and adapt some styles

--- api/og.js
+++ api/og.js
@@ -12,7 +12,7 @@ export default async (req, res) => {
     background: '#fff',
     fitTo: {
       mode: 'width',
-      value: 1200
+      value: 2400
     }
   }
 
@@ -20,26 +20,38 @@ export default async (req, res) => {
     res.arrayBuffer()
   )
 
+  const titleFontSize = title.length > 50 ? '96px' : '128px'
+
   const markup = html`<div
-    style="display: flex; flex-direction: column; width: 100vw; height: 100vh; padding: 64px;background-image: url('https://midu.dev/images/blur-background-01.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat;"
+    style="display: flex; position: relative; flex-direction: column; width: 100vw; height: 100vh;"
   >
     <img
-      id="logo"
-      src="https://midu.dev/images/solo-logo.png"
-      alt="Solo logo"
-      style="width: 150px;"
+      src="https://midu.dev/images/blur-background-01.jpg"
+      style="position:
+    absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;
+    object-position: center;"
     />
-    <img
-      id="tag"
-      src="https://midu.dev/images/tags/${tag}.png"
-      alt="${tag} logo"
-      style="width: 600px; position: absolute; bottom: -100px; right: -100px; opacity: .3;"
-    />
-    <h1
-      style="font-size: 128px; color: #111; max-width: 70%; text-align: left; line-height: 105%; letter-spacing: -2px;"
+    <div
+      style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; display: flex; flex-direction: column; padding: 64px;"
     >
-      ${title}
-    </h1>
+      <img
+        id="logo"
+        src="https://midu.dev/images/solo-logo.png"
+        alt="Solo logo"
+        style="width: 150px;"
+      />
+      <img
+        id="tag"
+        src="https://midu.dev/images/tags/${tag}.png"
+        alt="${tag} logo"
+        style="width: 400px; position: absolute; bottom: -70px; right: -70px; opacity: .4;"
+      />
+      <h1
+        style="font-size: ${titleFontSize}; color: #111; text-align: left; line-height: 105%; letter-spacing: -2px;"
+      >
+        ${title}
+      </h1>
+    </div>
   </div>`
 
   const svg = await satori(markup, {
@@ -63,57 +75,3 @@ export default async (req, res) => {
   res.setHeader('Content-Type', 'image/png')
   res.status(200).send(pngBuffer)
 }
-
-/*
-import chromium from 'chrome-aws-lambda'
-import playwright from 'playwright-core'
-
-const { NOW_REGION } = process.env
-const isDevelopment = NOW_REGION === 'dev1'
-const host = isDevelopment ? 'http://localhost:3000' : 'https://midu.dev'
-
-let emojiFontLoaded = false
-
-export default async (req, res) => {
-  if (!emojiFontLoaded) {
-    await chromium.font('https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf')
-    emojiFontLoaded = true
-  }
-
-  let browser = null
-  const { tag, subtitle, title } = req.query
-  console.log('New request with:')
-  console.log({ tag, subtitle, title })
-
-  console.log(await chromium.executablePath)
-
-  try {
-    if (!browser) {
-      browser = await playwright.chromium.launch({
-        args: chromium.args,
-        executablePath: await chromium.executablePath,
-        headless: chromium.headless
-      })
-    }
-
-    const context = await browser.newContext()
-    const page = await context.newPage()
-    console.log(`${host}/og/?tag=${tag}&${encodeURI(title)}`)
-    await page.goto(`${host}/og/?tag=${tag}&title=${title}&subtitle=${subtitle}`)
-    const screenshot = await page.screenshot({ type: 'png' })
-
-    res.setHeader('Content-Type', 'image/png')
-    res.status(200).send(screenshot)
-  } catch (error) {
-    console.error(error)
-    res.status(500).send({
-      status: 'Failed',
-      error
-    })
-  } finally {
-    if (browser !== null) {
-      await browser.close()
-    }
-  }
-}
-*/
